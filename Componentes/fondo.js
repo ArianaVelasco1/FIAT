3c42bd2053b7d0f83d5092c89cb95d4370a3f5e7
ArianaVelasco <velascoaelizabeth@gmail.com>
2022-09-14 16:44:08 -0500

    proyecto fiat

---
+++ Componentes/fondo.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css'
+
+export default function Fondo() {
+  return (
+    <div className={styles.diagonal}>
+    <div className={styles.container}>
+        
+    </div>
+    );
+    }
